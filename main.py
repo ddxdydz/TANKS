c269b165bdcad42b494a7c4612cb52dbddab2b6d
ddxdydz <73974596+ddxdydz@users.noreply.github.com>
2020-12-15 21:27:04 +0700

    init

---
+++ main.py
@@ -0,0 +1,509 @@
+import os
+import sys
+import pygame
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+TILE_SIZE = 40
+FPS = 15
+MAPS_DIR = "maps"
+
+# Control
+FORWARD = 91
+BACK = 92
+TURN_RIGHT = 93
+TURN_LEFT = 94
+TURN_RIGHT_TURRET = 95
+TURN_LEFT_TURRET = 96
+SHOOT = 97
+
+# Control keys dicts
+CONTROL_KEYS_V1 = \
+    {FORWARD: pygame.K_w, BACK: pygame.K_s,
+     TURN_RIGHT: pygame.K_d, TURN_LEFT: pygame.K_a,
+     TURN_RIGHT_TURRET: pygame.K_e, TURN_LEFT_TURRET: pygame.K_q,
+     SHOOT: pygame.K_r}
+CONTROL_KEYS_V2 = \
+    {FORWARD: pygame.K_g, BACK: pygame.K_b,
+     TURN_RIGHT: pygame.K_n, TURN_LEFT: pygame.K_v,
+     TURN_RIGHT_TURRET: pygame.K_h, TURN_LEFT_TURRET: pygame.K_f,
+     SHOOT: pygame.K_c}
+CONTROL_KEYS_V3 = \
+    {FORWARD: pygame.K_i, BACK: pygame.K_k,
+     TURN_RIGHT: pygame.K_j, TURN_LEFT: pygame.K_l,
+     TURN_RIGHT_TURRET: pygame.K_u, TURN_LEFT_TURRET: pygame.K_o,
+     SHOOT: pygame.K_p}
+CONTROL_KEYS_V4 = \
+    {FORWARD: pygame.K_UP, BACK: pygame.K_DOWN,
+     TURN_RIGHT: pygame.K_RIGHT, TURN_LEFT: pygame.K_LEFT,
+     TURN_RIGHT_TURRET: 44, TURN_LEFT_TURRET: 46, SHOOT: 47}
+
+DIRECTION_MOVE_BY_ANGLE = {0: (0, -1), 90: (-1, 0), 180: (0, 1), 270: (1, 0)}
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+
+    image = pygame.image.load(fullname).convert()
+
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Map:
+    def __init__(self, filename, free_tiles, image_tiles_dict):
+        self.map = []
+        with open(f"{MAPS_DIR}/{filename}") as input_file:
+            for line in input_file:
+                self.map.append(list(map(int, list(line.strip()))))
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.tile_size = TILE_SIZE
+        self.free_tiles = free_tiles
+
+        self.image_tiles_dict = image_tiles_dict
+
+    def render(self, screen):
+        screen.fill((59, 122, 87))
+        map_sprites = pygame.sprite.Group()
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.get_tile_id((x, y)) in self.image_tiles_dict.keys():
+                    sprite = pygame.sprite.Sprite(map_sprites)
+                    sprite.image = self.image_tiles_dict[self.get_tile_id((x, y))]
+                    sprite.rect = sprite.image.get_rect()
+                    sprite.rect.x = x * self.tile_size
+                    sprite.rect.y = y * self.tile_size
+        map_sprites.draw(screen)
+
+    def get_tile_id(self, position):
+        return self.map[position[1]][position[0]]
+
+    def is_free(self, position):
+        return self.get_tile_id(position) in self.free_tiles
+
+    def find_path_step(self, start, target):
+        INF = 1000
+        x, y = start
+        distance = [[INF] * self.width for _ in range(self.height)]
+        distance[y][x] = 0
+        prev = [[None] * self.width for _ in range(self.height)]
+        queue = [(x, y)]
+        while queue:
+            x, y = queue.pop(0)
+            for dx, dy in (1, 0), (0, 1), (-1, 0), (0, -1):
+                next_x, next_y = x + dx, y + dy
+                if 0 <= next_x < self.width and 0 < next_y < self.height and \
+                        self.is_free((next_x, next_y)) and distance[next_y][next_x] == INF:
+                    distance[next_y][next_x] = distance[y][x] + 1
+                    prev[next_y][next_x] = (x, y)
+                    queue.append((next_x, next_y))
+        x, y = target
+        if distance[y][x] == INF or start == target:
+            return start
+        while prev[y][x] != start:
+            x, y = prev[y][x]
+        return x, y
+
+
+class Tank(pygame.sprite.Sprite):
+    def __init__(self, position, image_turret, image_hull, crash_tank_image_turret, dict_id_bullets,
+                 rotate_turret=0, rotate_hull=0, control_keys=CONTROL_KEYS_V1, group=None):
+        super().__init__()
+
+        self.crash_tank_image_turret = crash_tank_image_turret
+        self.dict_id_bullets = dict_id_bullets
+
+        # init turret
+        self.tank_turret = pygame.sprite.Sprite()
+        self.tank_turret.image = image_turret
+        self.tank_turret.rect = self.tank_turret.image.get_rect()
+        self.tank_turret.rect.x, self.tank_turret.rect.y = \
+            position[0] * TILE_SIZE, position[1] * TILE_SIZE
+
+        # init hull
+        self.image = image_hull
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = \
+            position[0] * TILE_SIZE, position[1] * TILE_SIZE
+
+        self.rotate_turret = 0
+        self.set_turret_rotate(rotate_turret)
+        self.rotate_hull = 0
+        self.set_rotate(rotate_hull)
+        self.x, self.y = position
+
+        self.group = group
+        if self.group is not None:
+            self.group.add(self)
+            self.group.add(self.tank_turret)
+
+        self.control_keys = control_keys
+        self.is_crashed = False
+
+        # timers
+        self.shooting_cooldown = 40 * FPS
+        self.current_shooting_cooldown = 0
+
+        self.move_forward_cooldown = 8 * FPS
+        self.current_move_forward_cooldown = 0
+
+        self.move_back_cooldown = 10 * FPS
+        self.current_move_back_cooldown = 0
+
+        self.turn_cooldown = 20 * FPS
+        self.current_turn_cooldown = 0
+
+        self.turn_turret_cooldown = 30 * FPS
+        self.current_turn_turret_cooldown = 0
+
+    def update_timers(self, clock):
+        self.current_shooting_cooldown -= clock.get_time()
+        self.current_move_forward_cooldown -= clock.get_time()
+        self.current_move_back_cooldown -= clock.get_time()
+        self.current_turn_cooldown -= clock.get_time()
+        self.current_turn_turret_cooldown -= clock.get_time()
+
+    def get_position(self):
+        return self.x, self.y
+
+    def get_rotate(self):
+        return self.rotate_turret, self.rotate_hull
+
+    def set_control_keys(self, keys):
+        self.control_keys = keys
+
+    def set_position(self, position):
+        self.tank_turret.rect.x, self.tank_turret.rect.y = \
+            position[0] * TILE_SIZE, position[1] * TILE_SIZE
+        self.rect.x, self.rect.y = \
+            position[0] * TILE_SIZE, position[1] * TILE_SIZE
+        self.x, self.y = position
+
+    def set_turret_rotate(self, rotate):
+        self.rotate_turret = (self.rotate_turret + rotate) % 360
+        self.tank_turret.image = pygame.transform.rotate(self.tank_turret.image, rotate)
+
+    def set_rotate(self, rotate):
+        self.rotate_hull = (self.rotate_hull + rotate) % 360
+        self.image = pygame.transform.rotate(self.image, rotate)
+        self.set_turret_rotate(rotate)
+
+    def move_forward(self):
+        if self.current_move_forward_cooldown <= 0:
+            direction_move = DIRECTION_MOVE_BY_ANGLE[self.rotate_hull]
+            self.set_position((self.x + direction_move[0], self.y + direction_move[1]))
+            self.current_move_forward_cooldown = self.move_forward_cooldown
+            return True
+        return False
+
+    def move_back(self):
+        if self.current_move_back_cooldown <= 0:
+            direction_move = DIRECTION_MOVE_BY_ANGLE[self.rotate_hull]
+            self.set_position((self.x - direction_move[0], self.y - direction_move[1]))
+            self.current_move_back_cooldown = self.move_back_cooldown
+            return True
+        return False
+
+    def turn_right(self):
+        if self.current_turn_cooldown <= 0:
+            self.set_rotate(270)
+            self.current_turn_cooldown = self.turn_cooldown
+            return True
+        return False
+
+    def turn_left(self):
+        if self.current_turn_cooldown <= 0:
+            self.set_rotate(90)
+            self.current_turn_cooldown = self.turn_cooldown
+            return True
+        return False
+
+    def turn_turret_right(self):
+        if self.current_turn_turret_cooldown <= 0:
+            self.set_turret_rotate(270)
+            self.current_turn_turret_cooldown = self.turn_turret_cooldown
+            return True
+        return False
+
+    def turn_turret_left(self):
+        if self.current_turn_turret_cooldown <= 0:
+            self.set_turret_rotate(90)
+            self.current_turn_turret_cooldown = self.turn_turret_cooldown
+            return True
+        return False
+
+    def shoot(self, bullets_list):
+        if self.current_shooting_cooldown <= 0:
+            bullets_list.append(Bullet(
+                self.get_position(), self.rotate_turret, self.dict_id_bullets[0]))
+            self.current_shooting_cooldown = self.shooting_cooldown
+            return True
+        return False
+
+    def destroy_the_tank(self):
+        self.is_crashed = True
+        self.tank_turret.image = pygame.transform.rotate(
+            self.crash_tank_image_turret, self.get_rotate()[1])
+
+    def clear_the_tank(self):
+        self.group.remove(self)
+        self.group.remove(self.tank_turret)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, position, rotate, image):
+        super().__init__()
+        self.x, self.y = position
+        self.direction_move = DIRECTION_MOVE_BY_ANGLE[rotate]
+
+        self.image = pygame.transform.rotate(image, rotate)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = \
+            position[0] * TILE_SIZE, position[1] * TILE_SIZE
+
+        self.group = pygame.sprite.Group()
+        self.group.add(self)
+
+    def get_position(self):
+        return self.x, self.y
+
+    def get_direction_move(self):
+        return self.direction_move
+
+    def set_position(self, position):
+        self.x, self.y = position
+
+    def next_move(self):
+        self.x += self.direction_move[0]
+        self.y += self.direction_move[1]
+        self.rect.x, self.rect.y = \
+            self.x * TILE_SIZE, self.y * TILE_SIZE
+
+    def render(self, screen):
+        self.group.draw(screen)
+
+
+class Game:
+    def __init__(self, map, controlled_tanks, uncontrolled_tanks,
+                 bullets, clock, sprites_group=[]):
+        self.map = map
+        self.clock = clock
+
+        self.sprites_group = sprites_group
+        self.controlled_tanks = controlled_tanks
+        self.uncontrolled_tanks = uncontrolled_tanks
+
+        self.bullets = bullets
+
+    def render(self, screen):
+        self.map.render(screen)
+        self.update_bullets(screen)
+        for group in self.sprites_group:
+            group.draw(screen)
+
+    def update_bullets(self, screen):
+        for bullet in self.bullets:
+            bullet.next_move()
+            bullet_x, bullet_y = bullet.get_position()
+            if self.map.is_free((bullet_x, bullet_y)):
+                bullet.render(screen)
+            else:
+                del self.bullets[self.bullets.index(bullet)]
+                if isinstance(self.map.map[bullet_y][bullet_x], Tank):
+                    tank = self.map.map[bullet_y][bullet_x]
+                    if tank.is_crashed:
+                        self.map.map[bullet_y][bullet_x] = 0
+                        tank.clear_the_tank()
+                    else:
+                        tank.destroy_the_tank()
+
+    def update_controlled_tanks(self):
+        for tank in self.controlled_tanks:
+            tank.update_timers(self.clock)
+            if tank.is_crashed:
+                continue
+
+            cur_x, cur_y = next_x, next_y = tank.get_position()
+            rotate_turret, rotate_hull = tank.get_rotate()
+            direction_move = DIRECTION_MOVE_BY_ANGLE[rotate_hull]
+            self.map.map[cur_y][cur_x] = tank
+
+            if pygame.key.get_pressed()[tank.control_keys[FORWARD]]:
+                next_x += direction_move[0]
+                next_y += direction_move[1]
+                if self.map.is_free((next_x, next_y)):
+                    if tank.move_forward():
+                        self.map.map[cur_y][cur_x] = 0
+                        self.map.map[next_y][next_x] = tank
+            if pygame.key.get_pressed()[tank.control_keys[BACK]]:
+                next_x -= direction_move[0]
+                next_y -= direction_move[1]
+                if self.map.is_free((next_x, next_y)):
+                    if tank.move_back():
+                        self.map.map[cur_y][cur_x] = 0
+                        self.map.map[next_y][next_x] = tank
+            if pygame.key.get_pressed()[tank.control_keys[TURN_RIGHT]]:
+                tank.turn_right()
+            if pygame.key.get_pressed()[tank.control_keys[TURN_LEFT]]:
+                tank.turn_left()
+            if pygame.key.get_pressed()[tank.control_keys[TURN_RIGHT_TURRET]]:
+                tank.turn_turret_right()
+            if pygame.key.get_pressed()[tank.control_keys[TURN_LEFT_TURRET]]:
+                tank.turn_turret_left()
+            elif pygame.key.get_pressed()[tank.control_keys[SHOOT]]:
+                tank.shoot(self.bullets)
+
+    def update_uncontrolled_tanks(self):
+        for tank in self.uncontrolled_tanks:
+            tank.update_timers(self.clock)
+            if tank.is_crashed:
+                continue
+
+            cur_x, cur_y = tank.get_position()
+            rotate_turret, rotate_hull = tank.get_rotate()
+            direction_move = DIRECTION_MOVE_BY_ANGLE[rotate_hull]
+
+            # TODO оптимизировать алгоритм поведения автономных танков ниже
+            row_cells = self.map.map[cur_y].copy()
+            for cell in reversed(row_cells[:cur_x]):  # 90
+                if cell == 1:
+                    break
+                if isinstance(cell, Tank) and cell in self.controlled_tanks:
+                    if rotate_turret == 90:
+                        tank.shoot(self.bullets)
+                    elif rotate_turret == 0:
+                        tank.turn_turret_left()
+                    elif rotate_turret == 90:
+                        tank.turn_turret_right()
+                    elif rotate_turret == 180:
+                        tank.turn_turret_right()
+                    return None
+            for cell in row_cells[cur_x:]:  # 270
+                if cell == 1:
+                    break
+                if isinstance(cell, Tank) and cell in self.controlled_tanks:
+                    if rotate_turret == 270:
+                        tank.shoot(self.bullets)
+                    elif rotate_turret == 0:
+                        tank.turn_turret_right()
+                    elif rotate_turret == 270:
+                        tank.turn_turret_right()
+                    elif rotate_turret == 180:
+                        tank.turn_turret_left()
+                    return None
+            for cell in reversed([self.map.map[row][cur_x] for row in range(len(self.map.map)) if row < cur_x]):  # 0
+                if cell == 1:
+                    break
+                if isinstance(cell, Tank) and cell in self.controlled_tanks:
+                    if rotate_turret == 0:
+                        tank.shoot(self.bullets)
+                    elif rotate_turret == 90:
+                        tank.turn_turret_right()
+                    elif rotate_turret == 270:
+                        tank.turn_turret_left()
+                    elif rotate_turret == 180:
+                        tank.turn_turret_right()
+                    return None
+            for cell in [self.map.map[row][cur_x] for row in range(len(self.map.map)) if row > cur_x]:  # 180
+                if cell == 1:
+                    break
+                if isinstance(cell, Tank) and cell in self.controlled_tanks:
+                    if rotate_turret == 180:
+                        tank.shoot(self.bullets)
+                    elif rotate_turret == 0:
+                        tank.turn_turret_left()
+                    elif rotate_turret == 270:
+                        tank.turn_turret_right()
+                    elif rotate_turret == 90:
+                        tank.turn_turret_left()
+                    return None
+
+            next_position = self.map.find_path_step(
+                (cur_x, cur_y), self.controlled_tanks[0].get_position())
+            next_x, next_y = next_position
+
+
+def show_message(screen, message):
+    font = pygame.font.Font(None, 50)
+    text = font.render(message, True, (50, 70, 0))
+    text_x = WINDOW_WIDTH // 2 - text.get_width() // 2
+    text_y = WINDOW_HEIGHT // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    pygame.draw.rect(screen, (200, 150, 50), (text_x - 10, text_y - 10,
+                                              text_w + 20, text_h + 20))
+    screen.blit(text, (text_x, text_y))
+
+
+def init_test_scene(clock):
+    green_tank_turret = pygame.transform.scale(load_image(
+        "green_turret.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    green_tank_hull = pygame.transform.scale(load_image(
+        "green_hull.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    red_tank_turret = pygame.transform.scale(load_image(
+        "red_turret.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    red_tank_hull = pygame.transform.scale(load_image(
+        "red_hull.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    crash_tank = pygame.transform.scale(load_image(
+        "crached_turret.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    block_1 = pygame.transform.scale(load_image(
+        "block_1.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+    bullet_0 = pygame.transform.scale(load_image(
+        "bullet_0.png", colorkey=-1), (TILE_SIZE, TILE_SIZE))
+
+    # Формирование кадра(команды рисования на холсте):ww
+    main_map = Map("simple_map.txt", [0], {1: block_1})
+    all_sprites = pygame.sprite.Group()  # создадим группу, содержащую все спрайты
+
+    controlled_tanks = [
+        Tank((7, 1), green_tank_turret, green_tank_hull, crash_tank, {0: bullet_0},
+             rotate_turret=0, rotate_hull=180, group=all_sprites,
+             control_keys=CONTROL_KEYS_V1)]
+    uncontrolled_tanks = [
+        Tank((7, 7), red_tank_turret, red_tank_hull, crash_tank, {0: bullet_0},
+             rotate_turret=0, rotate_hull=0, group=all_sprites)]
+
+    bullets = []
+
+    return Game(main_map, controlled_tanks, uncontrolled_tanks,
+                bullets, clock, sprites_group=[all_sprites])
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption("TANK BATTLES")
+
+    # Главный игровой цикл:
+    clock = pygame.time.Clock()
+    game = init_test_scene(clock)
+    running = True
+    while running:
+        # Цикл приема и обработки сообщений:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            # обработка остальных событий
+            # ...
+
+        game.update_controlled_tanks()
+        game.update_uncontrolled_tanks()
+
+        game.render(screen)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
